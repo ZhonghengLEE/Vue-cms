606e63e5da7774fa7f488fa598da70ccf08314f7
ZhonghengLee <516454429@qq.com>
2018-11-22 22:02:53 +0800

    提交赶紧溜

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
 //入口文件
-// console.log('OK')
+  // console.log('OK')
 import Vue from 'vue'
+//1.1导入路由的包
+import VueRouter from 'vue-router'
+//1.2安装路由
+Vue.use(VueRouter)
 
 //按需导入 Mint-ui 组件
-//顶部固定栏
-import { Header } from 'mint-ui'
+  //顶部固定栏
+import { Header,Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+// Vue.config.productionTip = false
+//1.3插入抽离出去的路由模块
+import router from './router.js'
+
+//按需导入 mui组件
+    //tabbar 组件
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 
 //导入App 根组件
-import app from './app.vue'
+import app from './App.vue'
 
-var vm = new Vue({
+new Vue({
   el:'#app',
-  render: c => c(app)
+  render: c => c(app),
+  router //1.4挂载插入的路由对象到vm实例上
 })
